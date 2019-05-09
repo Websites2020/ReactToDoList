99355f68df68fc28eb07db5347b8d4399fa16618
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-09 00:21:56 -0400

    set up react, node, and express

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
