a8b6cd100e591be1c75c0391c76815f7646d6311
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-11 13:19:07 -0400

    set up heroku

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
 
 // npm run dev
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')));
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
