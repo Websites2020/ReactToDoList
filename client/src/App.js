760c6f5b617f11a03ed3082e9c508a529e6fbca4
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-11 12:50:08 -0400

    set up check button

--- client/src/App.js
+++ client/src/App.js
@@ -9,32 +9,143 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   this.state = {
-      input: "doesn't work"
+      input: [],
+      percent: 0,
+      number: 1,
+      checked: 0,
+      total: 0
     };
   this.taskInput = this.taskInput.bind(this);
+  this.check = this.check.bind(this);
+  this.del = this.del.bind(this);
    }
 
    taskInput = (taskValue) => {
       this.setState({
-        input: taskValue
+        input: this.state.input.concat(taskValue),
+        percent: (this.state.percent/this.state.number).toFixed(2),
+        number: this.state.number+1,
+        total: this.state.total+1
     })
    }
 
+   del(e) {
+    console.log(e.target.id);
+    let array = this.state.input
+    console.log(array[e.target.id])
+    array.splice(e.target.id, 1)
+    
+    console.log(array)
+    this.setState({
+      input: array,
+    })
+  }
+
+  check(e) {
+    console.log(e.target.id);
+    console.log(this.state.number)
+    console.log(this.state.total)
+    let x = document.getElementById(e.target.id)
+
+    if (x.className == "") {
+      this.setState({
+        checked: this.state.checked+1,
+      }, function updateCheckedPer() {
+        console.log(this.state.checked)
+        this.setState({
+          percent: ((this.state.checked/this.state.total)*100).toFixed(2)
+        })
+      }.bind(this))
+      x.className = "check";
+    } else {
+      this.setState({
+        checked: this.state.checked-1,
+        
+      }, function updateCheckedPer() {
+        console.log(this.state.checked)
+        this.setState({
+          percent: ((this.state.checked/this.state.total)*100).toFixed(2)
+        })
+      }.bind(this))
+      x.className = "";
+    }
+    
+    
+    // if (x.className == "") {
+    //   if (this.state.number != 1) {
+    //     this.setState({
+    //       percent: (100/(this.state.number-1)).toFixed(2),
+    //       number: this.state.number-1
+    //     })
+    //     x.className = "check";
+    //   } else {
+    //     if (this.state.number != 1) {
+    //       this.setState({
+    //         percent: (100/(this.state.number)).toFixed(2),
+    //         number: this.state.number-1
+    //       })
+    //       x.className = "check";
+    //     }
+    //   }
+    // } else {
+    //   if (this.state.total != this.state.number) {
+    //     this.setState({
+    //       number: this.state.number+1
+    //     }, function updatePer() {
+    //       console.log(this.state.number)
+    //       this.setState({
+    //         percent: (this.state.percent/this.state.number).toFixed(2),
+    //       })
+    //     }.bind(this))
+        
+    //     x.className = "";
+    //   } else {
+    //     this.setState({
+    //       percent: 0,
+    //       number: this.state.total-1
+    //     })
+    //     x.className = "";
+    //   }
+    // }    
+  }
+ 
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-3"></div>
           <div className="col-md-6">
-            <Input taskInput={this.taskInput} />
+            <br />
+              <Input taskInput={this.taskInput} />
+            <br />
           </div>
           <div className="col-md-3"></div>
         </div>
         <div className="row">
           <div className="col-md-12">
           
-            <Table insert={this.state.input} />
-          
+            
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Task</th>
+                  <th className="textRight" scope="col">Percent <span>{this.state.percent}</span>%</th>
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.input.map((text, i) =>
+                <tr key={i}>
+                  <td>{i+1}</td>
+                  <td id={i} className="">{text}</td>
+                  <td>
+                  <button className="right btn btn-danger" id={i} onClick={this.del}>&#10008;</button>
+                  <button className="right btn btn-success" id={i} onClick={this.check}>&#10004;</button>
+                  </td>
+                </tr>  
+              )}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -6,43 +6,25 @@ class Table extends React.Component {
       this.state = {
           percent: 100,
           number: 1,
-          text: ""
+          text: []
         };
         this.myRef = React.createRef();
       this.UNSAFE_componentWillReceiveProps = this.UNSAFE_componentWillReceiveProps.bind(this);
+      this.del = this.del.bind(this);
+      this.check = this.check.bind(this);
        }
 
        UNSAFE_componentWillReceiveProps() {
             this.setState({ 
                 number: this.state.number+1, 
                 percent: 100/this.state.number,
-                text: this.props.insert
+                text: this.state.text.concat(this.props.insert)
             }, function stateUpdateComplete() {
                         
                 console.log(this.state.number-1)
                 console.log(this.state.percent)
                 console.log(this.state.text)
 
-                const table = this.myRef.current;
-                var tr = document.createElement('tr');   
-                var td1 = document.createElement('td');
-                var td2 = document.createElement('td');
-                // var td3 = document.createElement('td');
-                var td4 = document.createElement('td');
-                var text1 = document.createTextNode(this.state.number-1);
-                var text2 = document.createTextNode(this.state.text);
-                // var text3 = document.createTextNode("");
-                var span = document.createElement('span');
-                span.innerHTML = `<button class="right btn btn-danger" id="${this.state.number-1}" onClick={this.del}>&#10008;</button> <button class="right btn btn-success" id="${this.state.number-1}" onClick={this.check}>&#10004;</button>`;
-                td1.appendChild(text1);
-                td2.appendChild(text2);
-                // td3.appendChild(text3);
-                td4.appendChild(span);
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                // tr.appendChild(td3);
-                tr.appendChild(td4);
-                table.appendChild(tr);
             }.bind(this))
         
        }
@@ -69,18 +51,11 @@ class Table extends React.Component {
     render() {
       return (
         <div>
-            <table class="table">
-                <thead class="thead-light">
-                    <tr>
-                        <th scope="col-md-1">#</th>
-                        <th scope="col-md-9">Task</th>
-                        <th scope="col-md-2" style={{textAlign: "right"}}>Percent: <span>{this.state.percent}</span>%</th>
-                    </tr>
-                </thead>
-                <tbody ref={this.myRef}>
-
-                </tbody>
-            </table>
+            <ol>
+            {this.state.text.map((text, i) => 
+                <li>{text[i]}</li>
+            )}
+            </ol>
         </div>
       )
     }
