757609c9cdbba5e011bd7c49ad177c4811481347
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-05-11 13:23:40 -0400

    addressed heroku error log issues

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const bodyParser = require('body-parser');
+const path = require('path');
 
 // npm run dev
 
